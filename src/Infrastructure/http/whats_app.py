d4b7043db3cd6ba85193ab64363fba92cc8c395d
JhonnyK10 <jhonnylino@gmail.com>
2025-03-15 22:26:51 -0300

    update whats

---
+++ src/Infrastructure/http/whats_app.py
@@ -0,0 +1,13 @@
+#from twilio.rest import Client
+
+#account_sid = 'ACCOUNT SID'
+#auth_token = 'AUTH'
+#client = Client(account_sid, auth_token)
+
+#verification = client.verify \
+#    .v2 \
+#    .services('SERVICE') \
+#    .verifications \
+#   .create(to='PHONE', channel='sms')
+
+#print(verification.sid)
