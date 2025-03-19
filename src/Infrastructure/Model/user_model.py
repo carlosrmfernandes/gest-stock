33da3678761b234b425b00f097600d14f596f58d
JhonnyK10 <jhonnylino@gmail.com>
2025-03-19 16:25:00 -0300

    add autoincrement to user id model

--- src/Infrastructure/Model/user_model.py
+++ src/Infrastructure/Model/user_model.py
@@ -4,7 +4,7 @@ from flask import jsonify, request
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
